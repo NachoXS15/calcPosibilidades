d5a83779d1f520289e77e0c402240e3ecef849cd
NachoXS-NET <pantojaignacio642@gmail.com>
2024-02-29 09:32:10 -0300

    initiated main function

---
+++ app.js
@@ -0,0 +1,8 @@
+const n = document.getElementById("n");
+const r = document.getElementById("r");
+const result = document.getElementById("result");
+const btnCalcular = document.getElementById("calcular");
+
+btnCalcular.addEventListener('click', e => {
+    
+})
